c7a9bfcb712c82dc784ecfa754489bcb8d547c7a
Franklin Chang <frnklnchng@gmail.com>
2018-07-12 15:44:39 -0700

    Add up until tracks

--- music_app/app/models/album.rb
+++ music_app/app/models/album.rb
@@ -1,3 +1,4 @@
 class Album < ApplicationRecord
   belongs_to :band
+  has_many :tracks
 end
--- music_app/config/routes.rb
+++ music_app/config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  get 'albums/new'
-  get 'albums/create'
-  get 'albums/edit'
-  get 'albums/show'
-  get 'albums/update'
-  get 'albums/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :new, :show]
   resource :session, only: [:create, :new, :destroy]
   resources :bands do
     resources :albums, only: [:new]
   end
-  resources :albums, only: [:show, :create, :edit, :update, :destroy]
+  resources :albums, only: [:show, :create, :edit, :update, :destroy] do
+    resources :tracks, only: [:new]
+  end
+  resources :tracks, only: [:create, :edit, :show, :update, :destroy]
 end
